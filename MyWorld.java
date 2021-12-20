a4468d327890fe4be98022ab90de225788d753e5
Keyin Altman <20655@my4county.net>
2021-12-20 11:05:28 -0500

    working

--- MyWorld.java
+++ MyWorld.java
@@ -17,21 +17,21 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 650, 1); 
-        
+
         Slime Slime = new Slime();
         addObject(Slime, 20, 595);
-        
+
         addObject( new Bean(),898, 455);
         addObject( new Bean(),207, 324);
         addObject( new Bean(),637, 549);
         addObject( new Bean(),379, 65);
         addObject( new Bean(),392, 340);
         addObject( new Bean(),78, 400);
-        
+
         addObject(new Home(), 943, 111);
-        
+
         addObject(new Seed(), 1, 221);
-        
+
         addObject(new Platform(), 107, 630);
         addObject( new Platform(), 324, 632);
         addObject( new Platform(),542, 594);
@@ -43,5 +43,13 @@ public class MyWorld extends World
         addObject( new Platform(),619, 184);
         addObject( new Platform(),836, 184);
         addObject( new Platform(),998, 184);
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
